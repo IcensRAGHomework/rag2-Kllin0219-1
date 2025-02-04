222f0995bea341348f509ccc203156262f740a47
guoliang_lin <guoliang_lin@asus.com>
2025-02-04 10:16:48 +0800

    hw

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,38 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    docs = loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_overlap=0)
+    chunks = text_splitter.split_documents(docs)
+    
+    return chunks[-1]
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    docs = loader.load()
+    
+    whole_text = "\n".join(doc.page_content for doc in docs)
+    
+    recursive_splitter = RecursiveCharacterTextSplitter(
+        chunk_overlap=0,
+        chunk_size=30,
+        separators=["\n第","\s第"],
+        is_separator_regex=True
+    )
+    
+    chunks = recursive_splitter.split_text(whole_text)
+    # for chunk in chunks:
+    #     print("===")
+    #     print(chunk)
+    # print(chunks[0])
+    return len(chunks)
+
+
+if __name__ == '__main__':
+    chunk = hw02_1(q1_pdf)
+    # print(chunk)
+    
+    chunk_count = hw02_2(q2_pdf)
+    print(chunk_count)
